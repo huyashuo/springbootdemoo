45296090dfd829c717d0fb117097eeeddc1bd798
humingsen <1623952061@qq.com>
2019-07-24 09:54:57 +0800

    员工管理

--- src/main/java/com/tyut/controller/HelloController.java
+++ src/main/java/com/tyut/controller/HelloController.java
@@ -19,7 +19,6 @@ public class HelloController {
 
     @RequestMapping("/success")
     public String success(Map<String,Object> map){
-        map.put("hello","<h1>你好</h1>");
         map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
         return "success";
     }
